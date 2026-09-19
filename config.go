be5fc505978d0a016df9dced07eb080e11056eb6
Khali <min427188@gmail.com>
2026-09-19 14:54:49 +0630

    configuration finsiehd loadFilename && loadConfig implemented

--- config.go
+++ config.go
@@ -1,1 +1,43 @@
 package goterminal
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type DevConfig struct {
+	Envs  []string `yaml:"envs"`
+	Hosts []string `yaml:"hosts"`
+}
+
+func LoadFile(filename string) ([]byte, error) {
+	if filename == "" {
+		filename = "devcheck.yaml"
+	}
+	byte, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	return byte, nil
+}
+
+func LoadConfig(filename string) (*DevConfig, error) {
+	byte, err := LoadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	//emty object instace
+	var cfg DevConfig
+
+	err = yaml.Unmarshal(byte, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("invalid YAML syntax: %w", err)
+	}
+	if len(cfg.Envs) == 0 && len(cfg.Hosts) == 0 {
+		return nil, fmt.Errorf("config file is empty or missing required sections")
+	}
+
+	return &cfg, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module devchecks
+
+go 1.27.1
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
